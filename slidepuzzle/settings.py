b1f8983a76e2e567f895fece892f4d7ff592405c
cesar-lextrait <cesar.lextrait@laplateforme.io>
2023-02-22 13:09:57 +0100

    slidepuzzle

---
+++ slidepuzzle/settings.py
@@ -0,0 +1,17 @@
+# colors (r, g, b)
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+BGCOLOUR = DARKGREY
+
+# game settings
+
+WIDTH = 1100
+HEIGHT = 641 
+FPS = 40
+title = "Slide Puzzle"
+TILESIZE = 128 
+#possibilité de varier la taille du puzzle en changeant le game_size. 
+GAME_SIZE = 3
